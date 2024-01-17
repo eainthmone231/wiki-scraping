8759ae3887374a1c4c17f426a09b95dbb53db9d3
La Min Ko Ko <69799049+SIRIUS-webkit@users.noreply.github.com>
2024-01-17 13:52:10 +0630

    update: remove unwanted css and unwanted data for data collection

--- content_scrape.py
+++ content_scrape.py
@@ -7,6 +7,24 @@ import argparse
 def is_interesting(tag):
     return tag.name in ['p', 'ol','ul']
 
+# get elements class to remove and reupdate the main content
+def remove_elements(soup, elements_to_remove):
+    for tag, attrs in elements_to_remove:
+       elements = soup.find_all(tag, attrs=attrs)
+       for element in elements:
+          element.decompose()
+          
+# remove unwanted class ul under 'reference list'
+def remove_adjacent_ul_under_class(soup, class_name):
+    main_elements = soup.find_all(class_=class_name)
+    for main_element in main_elements:
+        # Find all children of the main element
+        children = main_element.find_all_next('ul')
+
+        # Remove all children
+        for child in children:
+            child.decompose()
+
 def content_scrape(url,save_file):
     print("geeting from outer file" + url)
     response = requests.get(url)
@@ -14,7 +32,16 @@ def content_scrape(url,save_file):
     html_content = response.content.decode(encoding, 'replace') if encoding else response.text
     soup = BeautifulSoup(html_content,'html5lib')
     main_content = soup.find('div',attrs = {'class':'mw-content-ltr mw-parser-output'})
+
+    elements_to_remove = [
+        ('div', {'class': 'mw-references-wrap'}),
+        ('ul', {'class': 'gallery'}),
+        ('div',{'class': 'navbox'}),
+        ('div', {'class': 'catlinks'})
+    ]
     
+    remove_adjacent_ul_under_class(main_content, 'reflist') 
+    remove_elements(main_content, elements_to_remove)
 
     interesting_tags = main_content.find_all(is_interesting)
 
